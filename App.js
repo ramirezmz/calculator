fc3fc9d8460815ae4f191262fc4f79175fcc895c
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2021-03-22 21:09:25 -0300

    dando forma ao app

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from './src/components/Button'
-
+import Display from './src/components/Display'
 
 export default function App() {
+  state = {
+    displayValue: '0'
+  }
+  
+
   return (
     <View style={styles.container}>
+      <Display value={this.state.displayValue} />
       <View style={styles.buttons}>
         <Button label='AC' />
         <Button label='/' />
