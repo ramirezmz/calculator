1b73770fb4f22874ff9b20010603d71dfc474e9b
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2021-03-26 07:54:39 -0300

    done

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,42 +1,42 @@
 import React from 'react'
-import { 
+import {
     StyleSheet,
     Text,
     Dimensions,
     TouchableHighlight
- } from 'react-native'
+} from 'react-native'
 
- const styles = StyleSheet.create({
-     button: {
-         fontSize: 40,
-         height: Dimensions.get('window').width / 4,
-         width: Dimensions.get('window').width / 4,
-         padding: 20,
-         backgroundColor: '#f0f0f0',
-         textAlign: 'center',
-         borderWidth: 1,
-         borderColor: '#888',
-     },
-     operationButton: {
-         color: '#fff',
-         backgroundColor: '#fa8231',
-     },
-     buttonDouble: {
-         width: (Dimensions.get('window').width / 4) * 2,
-     },
-     buttonTriple:{
-         width: (Dimensions.get('window').width / 4) * 3,
-     } 
- })
+const styles = StyleSheet.create({
+    button: {
+        fontSize: 40,
+        height: Dimensions.get('window').width / 4,
+        width: Dimensions.get('window').width / 4,
+        padding: 20,
+        backgroundColor: '#f8f8f2',
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: '#44475a',
+    },
+    operationButton: {
+        color: '#8be9fd',
+        backgroundColor: '#ff5555',
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3,
+    }
+})
 
- export default props => {
-     const stylesButton = [styles.button]
-     if (props.double) stylesButton.push(styles.buttonDouble)
-     if (props.tiple) stylesButton.push(styles.buttonTriple)
-     if (props.operation) stylesButton.push(styles.operationButton)
-     return (
-         <TouchableHighlight onPress={() => props.onClick(props.label)}>
-             <Text style={stylesButton}>{props.label}</Text>
-         </TouchableHighlight>
-     )
- }
+export default props => {
+    const stylesButton = [styles.button]
+    if (props.double) stylesButton.push(styles.buttonDouble)
+    if (props.triple) stylesButton.push(styles.buttonTriple)
+    if (props.operation) stylesButton.push(styles.operationButton)
+    return (
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
+            <Text style={stylesButton}>{props.label}</Text>
+        </TouchableHighlight>
+    )
+}
